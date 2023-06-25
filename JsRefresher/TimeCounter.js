5157dbcfd23ebfbe4f1ebb46df1b42ce6ceb60aa
Azhar Ethan <96529107+Azharethan@users.noreply.github.com>
2023-06-25 23:57:41 +0530

    Update TimeCounter.js

--- JsRefresher/TimeCounter.js
+++ JsRefresher/TimeCounter.js
@@ -13,3 +13,6 @@ const currentTime = new Date();
 timer(currentTime);
 
 // clearInterval(timer(currentTime));
+//  the line currentTime.setSeconds(currentTime.getSeconds()+1) retrieves the current seconds value,
+increments it by one, and sets the updated value as the new seconds component of the currentTime object.
+This effectively increments the time by one second.
